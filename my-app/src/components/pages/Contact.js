d695587d513c6d55b8f276b0c7cd47d313188873
solaguirre <68126593+solaguirre@users.noreply.github.com>
2021-01-12 14:08:16 -0700

    Created Contact and Portfolio pages.

---
+++ my-app/src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+const Contact = () => (
+    <div>
+        <h1>Contact Me</h1>
+        <p>
+       <ul>
+           <li>Github</li>
+           <li>LinkedIn</li>
+           <li>Instagram</li>
+           <li>Twitter</li>
+       </ul>
+        </p>
+    </div>
+
+);
+
+export default Contact;
---
+++ my-app/src/components/pages/Portfolio.js
@@ -0,0 +1,16 @@
+const Portfolio = () => (
+    <div>
+        <h1>Contact Me</h1>
+        <p>
+       <ul>
+           <li>Github</li>
+           <li>LinkedIn</li>
+           <li>Instagram</li>
+           <li>Twitter</li>
+       </ul>
+        </p>
+    </div>
+
+);
+
+export default Portfolio;
