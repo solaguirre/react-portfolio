037f12ee82a5326edd93f3cbae3b26ff4bdff903
solaguirre <68126593+solaguirre@users.noreply.github.com>
2021-01-11 22:14:31 -0700

    Updated portfolio with AboutMe page.

---
+++ my-app/src/components/pages/AboutMe.js
@@ -0,0 +1,13 @@
+const AboutMe = () => (
+    <div>
+        <h1>About Me</h1>
+        <p>
+        Welcome! I am Marisol Aguirre from Tucson, Arizona.
+        A junior web developer currently taking courses in Full Stack Development at
+        the University of Arizona. I have experience working with people from all walks of life, especially interested in Early Childhood Education. I currently work as a Two-Way Dual Language Kindergarten teacher.
+        </p>
+    </div>
+
+);
+
+export default AboutMe;
