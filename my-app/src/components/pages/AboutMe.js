d1e23434d2ceaba270f617e2ba5cde63663e2f69
solaguirre <68126593+solaguirre@users.noreply.github.com>
2021-01-14 15:18:46 -0700

    Updated AboutMe file to include about me paragraph.

--- my-app/src/components/pages/AboutMe.js
+++ my-app/src/components/pages/AboutMe.js
@@ -1,13 +1,27 @@
-const AboutMe = () => (
-    <div>
-        <h1>About Me</h1>
-        <p>
-        Welcome! I am Marisol Aguirre from Tucson, Arizona.
-        A junior web developer currently taking courses in Full Stack Development at
-        the University of Arizona. I have experience working with people from all walks of life, especially interested in Early Childhood Education. I currently work as a Two-Way Dual Language Kindergarten teacher.
-        </p>
-    </div>
+import React from "react";
 
-);
+function AboutMe() {
+
+    return (
+        <div className="container" style={ }>
+            <div className="row">
+                <div className="col text-center">
+                    <h1 className="text-white" style={ }>About Me</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col text-center">
+                    <p>
+                        Welcome! I am Marisol Aguirre from Tucson, Arizona.
+                        I am a junior web developer currently taking courses in Full Stack Development at
+                        the University of Arizona. I have experience working with people from all walks of life, the last few years I have worked as a Two-Way Dual Language Kindergarten teacher.
+
+                        I am interested in pursuing my skills as a web developer and inspire a new generation of students to think like software engineers. My current technical skills include HTML5, CSS, Bootstrap, JavaScript, jQuery, JSON, AJAX, Node.js, Express.js, MySQL, Sequelize, MVC, MongoDB/Mongoose and React. </p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default AboutMe;
