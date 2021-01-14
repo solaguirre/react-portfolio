a604e199d6dd07d505d94d885fc5fab4d8f99f9d
solaguirre <68126593+solaguirre@users.noreply.github.com>
2021-01-14 15:28:43 -0700

    Created Home.js file and created buttons with paths to About Me and Portfolio pages.

--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -2,10 +2,25 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-function Home(){
+function Home() {
 
     return (
         <>
+            <div style={ }>
+                <div className="col text-center">
+                    <h1 className="">Marisol Aguirre</h1>p
+
+                <p>I am full-stack developer. Learn more about me and browse through my projects below.</p>
+
+                    <Link to="/AboutMe"><button className="btn btn-success button" style={ }>About Me</button></Link>
+
+                    <Link to="/Portfolio"><button className="btn btn-success button" style={ }>Portfolio</button></Link>
+                </div>
+
+
+
+
+            </div>
 
         </>
     )
